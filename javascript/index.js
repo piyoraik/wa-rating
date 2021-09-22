19b018ee4a6080fa537a5ea5152cfdd7ce99f431
piyoraik <42093052+piyoraik@users.noreply.github.com>
2021-09-22 23:11:59 +0900

    update validation

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,20 @@
+$.validator.addMethod(
+	// ルールの名前を指定
+	'regex',
+	// ルールのメソッド
+	function (value, element, regexp) {
+		return this.optional(element) || regexp.test(value)
+	},
+	// ルールに反した時のメッセージ
+	'半角数字で入力してください'
+)
+
 $(function () {
 	$('#form').validate({
 		rules: {
 			name: {
 				required: true,
+				maxlength: 20,
 			},
 			gender: {
 				required: true,
@@ -12,6 +24,14 @@ $(function () {
 			},
 			phone: {
 				required: true,
+				regex: /^[0-9]+$/,
+				minlength: 10,
+				maxlength: 11,
+			},
+			zipcode: {
+				regex: /^[0-9]+$/,
+				minlength: 7,
+				maxlength: 7,
 			},
 			address: {
 				maxlength: 50,
@@ -20,6 +40,7 @@ $(function () {
 		messages: {
 			name: {
 				required: '名前を入力してください',
+				maxlength: '20文字以内で入力してください',
 			},
 			gender: {
 				required: '選択してください',
@@ -29,6 +50,12 @@ $(function () {
 			},
 			phone: {
 				required: '電話番号を入力してください',
+				minlength: '10文字か11文字で入力してください',
+				maxlength: '10文字か11文字で入力してください',
+			},
+			zipcode: {
+				minlength: '郵便番号は7文字です',
+				maxlength: '郵便番号は7文字です',
 			},
 			address: {
 				maxlength: '50文字以内で入力してください',
